3f13de1d76fbdb1d2165736dbaa6c7bd92d0cf6a
Alexander Ustimenko <ustimenko.alexander@gmail.com>
2015-09-28 18:24:39 +0600

    Add dot and neato

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-php gimme-dot.php | dot -Tsvg > symfony.svg
+set -e
+
+php gimme-dot.php > symfony.dot.gv
+cat symfony.dot.gv | dot -Tsvg > symfony.dot.svg
+
+php gimme-neato.php > symfony.neato.gv
+cat symfony.neato.gv | neato -Tsvg > symfony.neato.svg
+
